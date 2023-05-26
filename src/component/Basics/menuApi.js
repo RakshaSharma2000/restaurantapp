b9f11d5e5c963d6c93b0b4ca321b954178975f13
Raksha <rakshasharma2022@vitbhopal.ac.in>
2023-05-26 10:24:21 +0530

    Initial commit

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,88 @@
+const Menu = [
+    {
+      id: 1,
+      image: "https://im.whatshot.in/img/2020/Sep/mag5-1599203552.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "Explore a range of MAGGI® Noodles like Masala Noodles, Atta Noodles, Chicken noodles, Oats Masala and cook delicious meals for family. 100% Safe for Consumption.",
+    },
+  
+    {
+      id: 2,
+      image: "https://thumbs.dreamstime.com/b/vada-di-aloo-o-bonda-di-aloo-78583494.jpg",
+      name: "allu pakoda",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Aloo pakora are soft melt-in-the-mouth fried potato fritters coated in a delicious, golden, crispy batter. It is also known as potato bajji and has always been one of my favorite tea-time snacks. Make this awesome vegan potato snack in less than 30 minutes.",
+    },
+    {
+      id: 3,
+      image: "https://images.herzindagi.info/image/2020/Jun/snacks-corn.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Packed with varied flavours of corn, pomegranate, orange, mustard and tamarind, you'll love the freshness of this corn recipe. This is a healthy bowl of meal that wouldn't take much of your time and is the most fuss-free recipe to make. Flavourful and healthy, the combination you shouldn't miss!",
+    },
+    {
+      id: 4,
+      image: "https://1.bp.blogspot.com/-9K0XYjBq3W8/XxFAO5t8e2I/AAAAAAAAKm4/PmWLH1shwzYugwZEI59yHNlfs2uTx1CHwCLcBGAsYHQ/s1920/chana-masala-1271639_1920.jpg",
+      name: "chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+              "Chola's Foods’ the essence from the western ghats of India, brings you a complete range of holistic organic dietary supplements to enhance the quality of your lives. The role of quality and hygiene of traditional (parampara) and ethnic foods has been increased in their consumption pattern with increasing demand from population.",
+    },
+    {
+      id: 5,
+      image:"https://images.pexels.com/photos/2619967/pexels-photo-2619967.jpeg?cs=srgb&dl=cheese-crust-delicious-2619967.jpg&fm=jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Flavorful Veg Kebab, crunchy Onion, Green Capsicum, Tomato & juicy Sweet Corn with delicious Tandoori Sauce and 100% mozzarella cheese.",
+    },
+    {
+      id: 6,
+      image: "https://im.whatshot.in/content/2017/Apr/1493300805-baramati-story.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "A thali is a combination of various dishes put together to make a whole meal. An average non-vegetarian thali usually contains salad, rice, chappati and gravy or dry chicken/mutton/fish dish to go with it.",
+    },
+    {
+      id: 7,
+      image: "https://i.redd.it/gvg2isjye4q01.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Indian sweet recipes - 170 Indian dessert recipes - includes kaju katli, gulab jamun, rasgulla, burfis, ladoos. Popular north Indian & South Indian sweets.",
+    },
+    {
+      id: 8,
+      image: "https://media.newstracklive.com/uploads/lifestyle-health/food-recipe/Jul/10/big_thumb/00_60e955a3d401a.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Rajma Recipe also known as Rajma masala is a North Indian curried dish made with kidney beans, onions, tomatoes, spices and herbs. This delicious dish is easy enough to make for a healthy and hearty meal. Serve it over chawal (rice) or with roti for a protein-packed vegetarian meal.",
+      
+    },
+    {
+      id: 9,
+      image: "http://giglee.in/wp-content/uploads/2017/10/maxresdefault-1.jpg",
+      name: "samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+        " A fried South Asian pastry with a savoury filling, including ingredients such as spiced potatoes, onions, and peas",
+    },
+  ];
+  
+  export default Menu;
+  
